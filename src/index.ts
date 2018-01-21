42bf6a06a57b64c654c02d1f8830e43ca3f68ea5
Timur Kiyui <timur@linux.com>
2018-01-22 01:19:15 +0800

    doc(usage): basic preview of plugin

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,100 @@ import { fromCustomEvent } from './methods/fromCustomEvent'
 import { watchProp } from './methods/watchProp'
 import { VueCycleConfiguration } from './index.d'
 
+/**
+ * # VueCycle
+ * A **Vue** plugin for injecting **Cycle.js** apps, batteries included.
+ *
+ * ### Features
+ * - A `vue-rx` style directive for DOM events
+ * - A custom, testable DOM source for Vue components
+ * - Injected helper methods that make writing Cycle-style components easier
+ *
+ * ## Usage
+ * VueCycle is currently not published on `npm` as it is still considered beta,
+ * and APIs may change over time. Consider it as `/dev/null` for now.
+ *
+ * ### Installing plugin
+ * VueCycle is available as a global plugin with very minimal configuration:
+ *
+ * ```javascript
+ * import VueCycle from '/dev/null'
+ *
+ * Vue.use(VueCycle, {
+ *   directive: 'xstream'
+ * })
+ * ```
+ *
+ * #### Composing VueCycle
+ * If you do not want to install VueCycle as a global plugin, you can compose
+ * and use parts of the plugin you want instead:
+ *
+ * ```javascript
+ * import { VueCycleMixin, VueCycleDomDirective } from '/dev/null'
+ *
+ * export default {
+ *   mixins: [VueCycleMixin],
+ *   directives: {
+ *     xstream: VueCycleDomDirective
+ *   }
+ * }
+ * ```
+ *
+ * ## Writing VueCycle Components
+ * The jist of what VueCycle does is that it injects a DOM driver, and a few
+ * convenience methods that makes writing Cycle-styled components possible.
+ *
+ * ```vue
+ * <template>
+ *   <p>Current value is: {{ value }}</p>
+ *   <button v-xstream:click="{ name: 'increment', data: count + 1 }">
+ *     Increment
+ *   </button>
+ * </template>
+ * <script>
+ * export default {
+ *   name: 'example',
+ *   data() {
+ *     return {
+ *       count: 42
+ *     }
+ *   },
+ *   $_cycle_app(sources) {
+ *     const increment$ = sources.DOM.on('increment')
+ *       .map(({ event, data }) => {
+ *         // Where `event` is the original `click` event, and data is the
+ *         // value passed to the directive
+ *         return data
+ *       })
+ *
+ *     const state$ = increment$
+ *       .map(newValue => {
+ *         this.count = newValue
+ *
+ *         return {
+ *           count: this.count
+ *         }
+ *       })
+ *
+ *     return {
+ *       state: state$
+ *     }
+ *   },
+ *   $_cycle_run(app) {
+ *     run(app, {
+ *       DOM: this.$domDriver,
+ *       state: state$ => state$.addListener({
+ *         next: () => {
+ *           // Empty example stub to make sure `state$` side-effects are run
+ *           // How you decide to handle state management is up to you =)
+ *         }
+ *       })
+ *     })
+ *   }
+ * }
+ * </script>
+ * ```
+ */
 const VueCycle: PluginObject<VueCycleConfiguration> = {
   install (Vue, config = { directive: 'xstream' }) {
     Vue.mixin(VueCycleDOMMixin)
