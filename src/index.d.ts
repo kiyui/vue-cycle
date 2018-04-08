5cd635d4a530fbfcddba5e4720b3487dbd7aeaa6
Timur Kiyui <timur@linux.com>
2018-04-08 21:29:06 +0800

    feat(types): improve typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,26 @@
 import { Stream } from 'xstream'
 
-declare module 'vue/types/vue' {
-  interface Vue {
-    $_dom_source: {
-      [key: string]: Stream<any>[];
-    };
-    $fromCustomEvent: Function;
-  }
+export interface DOMSourceEvent<S=Event, T=any> {
+  event: S;
+  data?: T;
 }
 
 export interface DOMSourceValue {
   name : string;
   data? : any;
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $_dom_source: {
+      [key: string]: Stream<any>[];
+    };
+    $fromCustomEvent<S=Event, T=any>(eventName: string): Stream<DOMSourceEvent<S,T>>;
+    $watchProp<T>(propName: string): Stream<T>;
+    $domDriver: {
+      on<S=Event, T=any>(eventName: string): Stream<DOMSourceEvent<S,T>>;
+    }
+  }
 }
 
 export interface VueCycleConfiguration {
